0cec07383104e2d5daf7b49cd14cb4023c8445c4
Ivan Kovalenko <s1ckretbox@gmail.com>
2020-06-30 21:40:56 +0300

    FDCAN: Add Init function
    Signed-off-by: Ivan Kovalenko <s1ckretbox@gmail.com>

--- sw-mcu/canfd-to-usb-nucleo/Core/Inc/utl_fdcan.h
+++ sw-mcu/canfd-to-usb-nucleo/Core/Inc/utl_fdcan.h
@@ -11,7 +11,13 @@
 #include "stm32g4xx_hal.h"
 
 #define FDCAN_PAYLOAD 64
+#define FDCAN_COUNT 3
 
 struct utl_fdcan_handle_t;
+
+/*
+ * Inits @fdcan_module, namely sets address of @fdcan_module
+ * */
+void utl_fdcan_init(struct utl_fdcan_handle_t ** fdcan_module, FDCAN_GlobalTypeDef * Instance, FDCAN_InitTypeDef * Init);
 
 #endif /* INC_UTL_FDCAN_H_ */
--- sw-mcu/canfd-to-usb-nucleo/Core/Src/utl_fdcan.c
+++ sw-mcu/canfd-to-usb-nucleo/Core/Src/utl_fdcan.c
@@ -14,3 +14,19 @@ struct utl_fdcan_handle_t {
   HAL_StatusTypeDef status_hal;
   uint8_t id;
 };
+
+static struct utl_fdcan_handle_t fdcan_modules[FDCAN_COUNT];
+
+static uint8_t handle_count = 0;
+
+static void utl_fdcan_error_handler(void);
+
+void utl_fdcan_init(struct utl_fdcan_handle_t ** fdcan_module, FDCAN_GlobalTypeDef * Instance, FDCAN_InitTypeDef * Init) {
+  fdcan_modules[handle_count].handle.Instance = Instance;
+  fdcan_modules[handle_count].handle.Init = *Init;
+
+  fdcan_modules[handle_count].status_hal = HAL_FDCAN_Init(&fdcan_modules[handle_count].handle);
+
+  *fdcan_module = &fdcan_modules[handle_count];
+  ++handle_count;
+}
